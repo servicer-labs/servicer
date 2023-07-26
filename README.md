7baec185f878f202d5ad576b2b9db9da472ad964
secretshardul <aeershardul@gmail.com>
2023-07-26 13:02:18 +0400

    feat: Build a CLI interface with clap

---
+++ README.md
@@ -0,0 +1,5 @@
+# Guide
+
+```sh
+cargo build && ./target/debug/stabled start 'hello-world.js' --name 'hello-world'
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use clap::{Parser, Subcommand};
+
+/// stabled process manager
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Turn an app into a service and start it
+    #[command(arg_required_else_help = true)]
+    Start {
+        /// The file path
+        path: String,
+
+        /// Optional custom name for the service
+        #[arg(short, long)]
+        name: Option<String>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match args.command {
+        Commands::Start { path , name} => {
+            println!("Running {}, {:?}", path, name);
+
+        },
+    }
+    println!("ok");
 }
