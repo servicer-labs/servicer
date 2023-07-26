b3331d9056508b30ac49cc9657115d13d848f10c
secretshardul <aeershardul@gmail.com>
2023-07-26 14:27:48 +0400

    feat: Check file existence, find interpreter

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use clap::{Parser, Subcommand};
+use std::ffi::OsStr;
+use std::fs;
+use std::path::Path;
 
 /// stabled process manager
 #[derive(Parser, Debug)]
@@ -18,7 +21,11 @@ enum Commands {
 
         /// Optional custom name for the service
         #[arg(short, long)]
-        name: Option<String>
+        name: Option<String>,
+
+        /// Optional custom interpreter. By default `node` is used for .js and `python3` for .py
+        #[arg(short, long)]
+        interpreter: Option<String>,
     }
 }
 
@@ -26,10 +33,63 @@ fn main() {
     let args = Args::parse();
 
     match args.command {
-        Commands::Start { path , name} => {
-            println!("Running {}, {:?}", path, name);
+        Commands::Start { path: path_as_string , name: custom_name, interpreter: custom_interpreter} => {
+            println!("Running {}, {:?}", path_as_string, custom_name);
 
+            // Step 1. Check whether the file exists at path
+            let path = Path::new(&path_as_string);
+            if !path.exists() {
+                panic!("Could not find file at path {}", path.to_str().unwrap_or_default());
+            }
+
+            if !path.is_file() {
+                panic!("A non-file entity (e.g., directory) exists at the path {}", path.to_str().unwrap_or_default());
+            }
+
+            // The file name including extension
+            let file_name = path
+                .file_name().expect("Failed to get file name")
+                .to_str().expect("Failed to stringify file name");
+
+            println!("File name: {}", file_name);
+
+            // Step 2. Get interpreter
+
+            let interpreter = match custom_interpreter {
+                Some(_) => custom_interpreter,
+                None => get_interpreter(path.extension())
+            };
+            println!("Got interpreter {:?}", interpreter);
+
+            // Step 3. Create service file
         },
     }
     println!("ok");
+}
+
+/// Find the interpreter needed to execute a file with the given extension
+///
+/// # Arguments
+///
+/// * `extension`: The file extension
+///
+fn get_interpreter(extension: Option<&OsStr>) -> Option<String> {
+    match extension {
+        Some(extension_os_str) => {
+            let extension_str = extension_os_str.to_str().expect("failed to stringify extension");
+
+            let interpreter = match extension_str {
+                "js" => "node",
+                "py" => "python3",
+                _ => panic!("No interpeter found for extension {}. Please provide a custom interpeter and try again.", extension_str)
+            };
+
+            Some(interpreter.to_string())
+        },
+        None => None
+    }
+}
+
+fn create_service_file(name: &str, interpreter: Option<String>) {
+
 }
