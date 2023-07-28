2732274d6fa1bfde262bb61d4f0c3698f7081eff
secretshardul <aeershardul@gmail.com>
2023-07-28 17:43:03 +0400

    feat: Check for existant service, launch service

--- README.md
+++ README.md
@@ -18,3 +18,92 @@ cargo build && ./target/debug/stabled start 'hello-world.js' --name 'hello-world
 
   - Option 3: Use `n` instead of `nvm`
 
+## D-bus concepts
+
+A dbus service has 3 parts:
+
+1. Name: The service name, i.e. `org.freedesktop.Notifications`
+2. Object: An instance to interact with the service. Eg `org/freedesktop/Notifications`
+3. Interface: XML ABI to interact with service, eg
+
+
+```sh
+# Start
+## ss is the signature, i.e. two strings must be passed.
+busctl call \
+  org.freedesktop.systemd1 \
+  /org/freedesktop/systemd1 \
+  org.freedesktop.systemd1.Manager \
+  StartUnit ss \
+  "hello.service" \
+  "replace"
+
+busctl call \
+  org.freedesktop.systemd1 \
+  /org/freedesktop/systemd1 \
+  org.freedesktop.systemd1.Manager \
+  GetDefaultTarget
+
+# Scripts to read PID, RAM and other details are in the next section
+sudo systemctl list-units > list-units.txt
+
+busctl call org.freedesktop.systemd1 /org/freedesktop/systemd1 org.freedesktop.systemd1.Manager ListUnits
+
+#TODO find equivalent of `systemctl show`. The below scripts don't work
+
+# Get status of hello-world, equivalent of `sudo systemctl status hello-world.stabled`
+busctl get-property org.freedesktop.systemd1 /org/freedesktop/systemd1/unit/hello_2dworld_2establed_2eservice org.freedesktop.systemd1.Unit ActiveState
+
+
+# Stop
+```
+
+- `ListUnits` output for hello-world
+
+```
+"hello-world.stabled.service" "stabled: hello-world" "loaded" "active" "running" "" "/org/freedesktop/systemd1/unit/hello_2dworld_2establed_2eservice"
+```
+
+- Naming convention of paths: `/` and `_` are ok
+- Other characters are encoded as lowercase HEX, eg `-` becomes `_2d`
+- How is `_` escaped?
+
+```
+  hello-world.stabled.service                                                                loaded active     running   stabled: hello-world
+```
+
+- `sudo systemctl status hello-world.stabled` returns a bunch of details. Unfortunately it doesn't expose a DBUS API to read RAM and CPU. We use two objects `org.freedesktop.systemd1.Unit` and `org.freedesktop.systemd1.Service` to read fields.
+
+  - Loaded state, active state and sub state (alive/dead): `busctl get-property org.freedesktop.systemd1 /org/freedesktop/systemd1/unit/hello_2dworld_2establed_2eservice org.freedesktop.systemd1.Unit LoadState ActiveState SubState`
+
+  - PID: This is found on the service unit object, i.e. `.Service` not `.Unit`. `busctl get-property org.freedesktop.systemd1 /org/freedesktop/systemd1/unit/hello_2dworld_2establed_2eservice org.freedesktop.systemd1.Service MainPID`
+
+  - Memory and CPU: read from `/proc/{pid}/stat`. Use crate https://github.com/rust-psutil/rust-psutil
+  - Logs
+
+- Source code: https://github.com/systemd/systemd/blob/4cf5b343c927509ea91cf56ca88e330f193a6963/src/systemctl/systemctl-show.c#L713
+
+```
+● hello-world.stabled.service - stabled: hello-world
+     Loaded: loaded (/etc/systemd/system/hello-world.stabled.service; disabled; preset: enabled)
+     Active: active (running) since Wed 2023-07-26 18:57:54 +04; 10min ago
+   Main PID: 25875 (node)
+      Tasks: 7 (limit: 18621)
+     Memory: 10.2M
+        CPU: 159ms
+     CGroup: /system.slice/hello-world.stabled.service
+             └─25875 /usr/local/bin/node index.js
+
+Jul 26 18:57:54 hp systemd[1]: Started hello-world.stabled.service - stabled: hello-world.
+Jul 26 18:57:54 hp node[25875]: Server listening on port 3000
+```
+
+# CLI behavior
+
+- `start`: The input can be a file path or a service name.
+  - If it is a file path, a new service is created.
+  - If it is a service name, restart the service if it has been stopped.
+  - If it is a file path and a service with the given name already exists, overwrite that service.
+  - Occam's Razor- simple, people already familiar.
+
+- Alternative with `init` and `start`: pm2 method is convenient for redeploying apps by using `-f`. To replicate this, have an `overwrite` param. If a service with the same name exists, it gets overwritten.
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use clap::{Parser, Subcommand};
 use indoc::formatdoc;
-use which::which;
 use std::ffi::OsStr;
 use std::io::Write;
-use std::{fs, env};
 use std::path::Path;
+use std::{env, fs};
+use which::which;
+use zbus::blocking::Connection;
+use zbus::dbus_proxy;
+
+const TOOL_NAME: &str = "stabled";
 
 /// stabled process manager
 #[derive(Parser, Debug)]
@@ -16,23 +20,26 @@ struct Args {
 
 #[derive(Debug, Subcommand)]
 enum Commands {
-    /// Turn an app into a service and start it. Must be called in `sudo` mode
+    /// Daemonize an app at a given path or start an existing service
     #[command(arg_required_else_help = true)]
     Start {
-        /// The file path
-        path: String,
+        /// The file path or service to start
+        path_or_service: String,
 
-        /// Optional custom name for the service
+        /// Optional custom name for the daemon
         #[arg(short, long)]
         name: Option<String>,
 
         /// Optional custom interpreter. By default `node` is used for .js and `python3` for .py
         #[arg(short, long)]
         interpreter: Option<String>,
-    }
+
+        #[arg(short, long, default_value_t = false)]
+        force: bool,
+    },
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
     // TODO exit if systemd is not installed
@@ -41,51 +48,95 @@ fn main() {
 
     match args.command {
         Commands::Start {
-            path: path_as_string ,
+            path_or_service,
             name: custom_name,
-            interpreter: custom_interpreter
+            interpreter: custom_interpreter,
+            force,
         } => {
-            println!("Running {}, {:?}", path_as_string, custom_name);
+            let connection = zbus::blocking::Connection::system().unwrap();
 
-            // Step 0. Check if service already exists
-
-            // Step 1. Check whether the file exists at path
-            let path = Path::new(&path_as_string);
-            if !path.exists() {
-                panic!("Could not find file at path {}", path.to_str().unwrap_or_default());
-            }
-
-            if !path.is_file() {
-                panic!("A non-file entity (e.g., directory) exists at the path {}", path.to_str().unwrap_or_default());
-            }
-
-            // The file name including extension
-            let file_name = path
-                .file_name().expect("Failed to get file name")
-                .to_str().expect("Failed to stringify file name").to_string();
-
-            println!("File name: {}", file_name);
-
-            // Step 2. Get interpreter
-
-            let interpreter = match custom_interpreter {
-                Some(_) => custom_interpreter,
-                None => get_interpreter(path.extension())
+            // Does user provide a unit name to start an existing service?
+            let full_service_name = if path_or_service.ends_with(&format!("{TOOL_NAME}.service")) {
+                path_or_service.clone()
+            } else {
+                format!("{path_or_service}.{TOOL_NAME}.service")
             };
-            println!("Got interpreter {:?}", interpreter);
+            let load_state = get_load_state(&full_service_name, &connection);
 
-            // Step 3. Create service file
-            let service_name = custom_name.unwrap_or(file_name.to_string());
+            if load_state == "invalid-unit-path" || load_state == "not-found" {
+                // User provided a file path
+                let file_path = Path::new(&path_or_service);
+                if !file_path.exists() {
+                    panic!(
+                        "Could not find file at path {}",
+                        file_path.to_str().unwrap_or_default()
+                    );
+                }
 
-            let working_directory = fs::canonicalize(path.parent().unwrap()).unwrap()
-                .to_str().unwrap().to_string();
+                if !file_path.is_file() {
+                    panic!(
+                        "A non-file entity (e.g., directory) exists at the path {}",
+                        file_path.to_str().unwrap_or_default()
+                    );
+                }
 
-            println!("got working directory {:?}", working_directory);
+                // The file name including extension
+                let file_name = file_path
+                    .file_name()
+                    .expect("Failed to get file name")
+                    .to_str()
+                    .expect("Failed to stringify file name")
+                    .to_string();
 
-            create_service_file(&service_name, &working_directory, interpreter, &file_name).unwrap();
-        },
+                let service_name = custom_name.unwrap_or(file_name.to_string());
+                let full_service_name = format!("{}.{}.service", service_name, TOOL_NAME);
+
+                let active_state = get_active_state(&full_service_name, &connection);
+                if active_state == "active" || active_state == "reloading" {
+                    if !force {
+                        eprintln!("A unit named {} is {}. Run with --force true to overwrite", full_service_name, active_state);
+                        return Ok(());
+                    }
+                    println!("Overwriting unit");
+                }
+
+                // Create file if it doesn't exist
+                let service_file_path = format!("/etc/systemd/system/{}", full_service_name.clone());
+                if !Path::new(&service_file_path).exists() || force {
+                    let interpreter = match custom_interpreter {
+                        Some(_) => custom_interpreter,
+                        None => get_interpreter(file_path.extension()),
+                    };
+
+                    let working_directory = fs::canonicalize(file_path.parent().unwrap())
+                        .unwrap()
+                        .to_str()
+                        .unwrap()
+                        .to_string();
+
+                    create_service_file(
+                        &service_name,
+                        &service_file_path,
+                        &working_directory,
+                        interpreter,
+                        &file_name
+                    )
+                        .unwrap();
+                }
+
+                // Start service
+                let manager_proxy = ManagerProxyBlocking::new(&connection).unwrap();
+                let start_service_result = manager_proxy.start_unit(full_service_name.clone(), "replace".into())
+                    .expect(&format!("Failed to start service {}", full_service_name));
+                println!("start service result {start_service_result}");
+            } else {
+                // Start an existing service
+            }
+        }
     }
     println!("ok");
+
+    Ok(())
 }
 
 /// Find the interpreter needed to execute a file with the given extension
@@ -97,7 +148,9 @@ fn main() {
 fn get_interpreter(extension: Option<&OsStr>) -> Option<String> {
     match extension {
         Some(extension_os_str) => {
-            let extension_str = extension_os_str.to_str().expect("failed to stringify extension");
+            let extension_str = extension_os_str
+                .to_str()
+                .expect("failed to stringify extension");
 
             let interpreter = match extension_str {
                 "js" => "node",
@@ -106,16 +159,17 @@ fn get_interpreter(extension: Option<&OsStr>) -> Option<String> {
             };
 
             Some(interpreter.to_string())
-        },
-        None => None
+        }
+        None => None,
     }
 }
 
-/// Creates a systemd service file at `/etc/systemd/system/{}.stabled.service`
+/// Creates a systemd service file at `/etc/systemd/system/{}.stabled.service` and returns the unit name
 ///
 /// # Arguments
 ///
-/// * `service_name`- Name of the service
+/// * `service_name`- Name of the service without '.stabled.service' in the end
+/// * `service_file_path` - Path where the service file will be written
 /// * `working_directory` - Working directory of the file to execute
 /// * `interpreter` - The executable used to run the app, eg. `node` or `python3`. The executable
 /// must be visible from path for a sudo user. Note that the app itself does not run in sudo.
@@ -124,14 +178,15 @@ fn get_interpreter(extension: Option<&OsStr>) -> Option<String> {
 ///
 fn create_service_file(
     service_name: &String,
+    service_file_path: &String,
     working_directory: &String,
     interpreter: Option<String>,
-    file_name: &String
+    file_name: &String,
 ) -> std::io::Result<()> {
-    const TOOL_NAME: &str = "stabled";
 
     // This gets `root` instead of `hp` if sudo is used
-    let user = env::var("SUDO_USER").expect("Must be in sudo mode. ENV variable $SUDO_USER not found");
+    let user =
+        env::var("SUDO_USER").expect("Must be in sudo mode. ENV variable $SUDO_USER not found");
     let exec_start = match interpreter {
         Some(interpreter) => {
             // Find full path of interpreter
@@ -139,52 +194,136 @@ fn create_service_file(
             // readable in sudo. python3 works out of the box but nvm requires a hack.
             let interpreter_path = which(&interpreter)
                 .expect(&format!("Could not find executable for {}", interpreter))
-                .to_str().expect(&format!("Failed to stringify interpreter path for {}.", interpreter))
+                .to_str()
+                .expect(&format!(
+                    "Failed to stringify interpreter path for {}.",
+                    interpreter
+                ))
                 .to_string();
 
             format!("{} {}", interpreter_path, file_name)
-        },
-        None => file_name.clone()
+        }
+        None => file_name.clone(),
     };
 
     // Replacement for format!(). This proc macro removes spaces produced by indentation.
     let service_body = formatdoc! {
         r#"
-        # This file was generated by {}. Do not edit unless you know what you are doing.
+        # This file was generated by {TOOL_NAME}. Do not edit unless you know what you are doing.
         [Unit]
-        Description={}: {}
+        Description={TOOL_NAME}: {service_name}
         After=network.target
 
         [Service]
         Type=simple
-        User={}
+        User={user}
 
-        WorkingDirectory={}
-        ExecStart={}
+        WorkingDirectory={working_directory}
+        ExecStart={exec_start}
 
         [Install]
         WantedBy=multi-user.target
-        "#,
-        TOOL_NAME,
-        TOOL_NAME,
-        service_name,
-        user,
-        working_directory,
-        exec_start
+        "#
     };
 
+    println!("Creating service file {service_file_path}");
     println!("{}", service_body);
-
-    let service_file_path = format!("/etc/systemd/system/{}.{}.service", service_name, TOOL_NAME);
-
-    if Path::new(&service_file_path).exists() {
-        panic!("Service {} is already deployed at {}", service_name, service_file_path);
-    }
 
     // Create the service file and write the content
     let mut file = fs::File::create(service_file_path)?;
     file.write_all(service_body.as_bytes())?;
 
     Ok(())
+}
 
+/// Proxy object for `org.freedesktop.systemd1.Manager`.
+/// Taken from https://github.com/lucab/zbus_systemd/blob/main/src/systemd1/generated.rs
+#[dbus_proxy(
+    interface = "org.freedesktop.systemd1.Manager",
+    default_service = "org.freedesktop.systemd1",
+    default_path = "/org/freedesktop/systemd1"
+)]
+trait Manager {
+    /// [📖](https://www.freedesktop.org/software/systemd/man/systemd.directives.html#StartUnit()) Call interface method `StartUnit`.
+    #[dbus_proxy(name = "StartUnit")]
+    fn start_unit(
+        &self,
+        name: String,
+        mode: String,
+    ) -> zbus::Result<zbus::zvariant::OwnedObjectPath>;
+}
+
+/// Proxy object for `org.freedesktop.systemd1.Unit`.
+#[dbus_proxy(
+    interface = "org.freedesktop.systemd1.Unit",
+    default_service = "org.freedesktop.systemd1",
+    gen_blocking = true,
+    // No default path. Path depends on service name, eg /org/freedesktop/systemd1/unit/hello_2dworld_2establed_2eservice
+    assume_defaults = false
+)]
+trait Unit {
+    /// Get property `ActiveState`.
+    #[dbus_proxy(property, name = "ActiveState")]
+    fn active_state(&self) -> zbus::Result<String>;
+
+    /// Get property `LoadState`.
+    #[dbus_proxy(property)]
+    fn load_state(&self) -> zbus::Result<String>;
+}
+
+/// Returns the load state of a systemd unit
+///
+/// Returns `invalid-unit-path` if the path is invalid
+///
+/// # Arguments
+///
+/// * `full_service_name`: Full name of the service name with '.service' in the end
+/// * `connection`: Blocking zbus connection
+///
+fn get_load_state(full_service_name: &String, connection: &Connection) -> String {
+    let object_path = format!("/org/freedesktop/systemd1/unit/{}", encode_as_dbus_object_path(full_service_name));
+    println!("object path {object_path}");
+
+    match zbus::zvariant::ObjectPath::try_from(object_path) {
+        Ok(path) => {
+            let unit_proxy = UnitProxyBlocking::new(connection, path).unwrap();
+            unit_proxy.load_state().unwrap_or("invalid-unit-path".into())
+        }
+        Err(_) => "invalid-unit-path".to_string()
+    }
+}
+
+/// Returns the load state of a systemd unit
+///
+/// Returns `invalid-unit-path` if the path is invalid
+///
+/// # Arguments
+///
+/// * `full_service_name`: Full name of the service name with '.service' in the end
+/// * `connection`: Blocking zbus connection
+///
+fn get_active_state(full_service_name: &String, connection: &Connection) -> String {
+    let object_path = format!("/org/freedesktop/systemd1/unit/{}", encode_as_dbus_object_path(full_service_name));
+    println!("object path {object_path}");
+
+    match zbus::zvariant::ObjectPath::try_from(object_path) {
+        Ok(path) => {
+            let unit_proxy = UnitProxyBlocking::new(connection, path).unwrap();
+            unit_proxy.active_state().unwrap_or("invalid-unit-path".into())
+        }
+        Err(_) => "invalid-unit-path".to_string()
+    }
+}
+
+fn encode_as_dbus_object_path(input_string: &str) -> String {
+    input_string
+        .chars()
+        .map(|c| {
+            if c.is_ascii_alphanumeric() || c == '/' || c == '_' {
+                c.to_string()
+            } else {
+                format!("_{:x}", c as u32)
+            }
+        })
+        .collect()
 }
